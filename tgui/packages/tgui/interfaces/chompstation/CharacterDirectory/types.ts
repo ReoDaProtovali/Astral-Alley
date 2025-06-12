ebbac5ed10f55d7c3320d55e67d1fd89c0a49497
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2025-06-12 16:28:31 -0700

    [MIRROR] Add character portraits to the character directory

--- tgui/packages/tgui/interfaces/chompstation/CharacterDirectory/types.ts
+++ tgui/packages/tgui/interfaces/chompstation/CharacterDirectory/types.ts
@@ -26,4 +26,9 @@ export type mobEntry = {
   erptag: string;
   character_ad: string;
   flavor_text: string;
+<<<<<<< HEAD:tgui/packages/tgui/interfaces/chompstation/CharacterDirectory/types.ts
+=======
+  custom_link: string;
+  photo: string | null;
+>>>>>>> d26a83b799 ([MIRROR] Add character portraits to the character directory (#11056)):tgui/packages/tgui/interfaces/CharacterDirectory/types.ts
 };
