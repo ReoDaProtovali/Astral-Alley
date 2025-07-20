0caff4cd78d20d77c9c5c03956fae4aec21cde59
CHOMPStation2StaffMirrorBot <94713762+CHOMPStation2StaffMirrorBot@users.noreply.github.com>
2025-07-20 07:29:37 -0700

    [MIRROR] simple ghost pod find

--- tgui/packages/tgui/interfaces/RobotChoose/ModuleSection.tsx
+++ tgui/packages/tgui/interfaces/RobotChoose/ModuleSection.tsx
@@ -20,6 +20,7 @@ export const ModuleSection = (props: {
   const filtered = sortable?.filter(searcher);
 
   return (
+<<<<<<< HEAD
     <Section title={title} fill scrollable width="30%">
       <Stack.Item mb={'10px'}>
         <Input
@@ -42,6 +43,35 @@ export const ModuleSection = (props: {
             ))}
         </Stack>
       </Stack.Item>
+=======
+    <Section title={title} fill>
+      <Stack vertical fill>
+        <Stack.Item>
+          <Input
+            fluid
+            value={searchText}
+            placeholder="Search for modules..."
+            onChange={(value: string) => setSearchText(value)}
+          />
+        </Stack.Item>
+        <Stack.Divider />
+        <Stack.Item grow>
+          <Section fill scrollable>
+            <Stack vertical fill>
+              {!!filtered &&
+                filtered.map((filter) => (
+                  <SelectorElement
+                    key={filter}
+                    option={filter}
+                    action="pick_module"
+                    selected={selected}
+                  />
+                ))}
+            </Stack>
+          </Section>
+        </Stack.Item>
+      </Stack>
+>>>>>>> 47c7add324 ([MIRROR] simple ghost pod find (#11205))
     </Section>
   );
 };
